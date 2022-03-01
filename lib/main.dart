43b986049adadf1791163c3b0ab8f38e9d0d3242
zarp1999 <zarp.191@gmail.com>
2022-03-01 10:08:29 +0800

    sun,moon,star list

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,115 @@ void main(){
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final europeanCountries = [    'Albania',
+    'Andorra',
+    'Armenia',
+    'Austria',
+    'Azerbaijan',
+    'Belarus',
+    'Belgium',
+    'Bosnia and Herzegovina',
+    'Bulgaria',
+    'Croatia',
+    'Cyprus',
+    'Czech Republic',
+    'Denmark',
+    'Estonia',
+    'Finland',
+    'France',
+    'Georgia',
+    'Germany',
+    'Greece',
+    'Hungary',
+    'Iceland',
+    'Ireland',
+    'Italy',
+    'Kazakhstan',
+    'Kosovo',
+    'Latvia',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macedonia',
+    'Malta',
+    'Moldova',
+    'Monaco',
+    'Montenegro',
+    'Netherlands',
+    'Norway',
+    'Poland',
+    'Portugal',
+    'Romania',
+    'Russia',
+    'San Marino',
+    'Serbia',
+    'Slovakia',
+    'Slovenia',
+    'Spain',
+    'Sweden',
+    'Switzerland',
+    'Turkey',
+    'Ukraine',
+    'United Kingdom',
+    'Vatican City'
+    ];
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My List"),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            elevation: 20,
+            child: ListTile(
+              leading: Icon(Icons.wb_sunny),
+              title: Text('Sun'),
+              subtitle: Text('hot and yellow'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: (){
+                print('sun');
+              },
+            ),
+          ),
+           Card(
+             elevation: 20,
+             child: ListTile(
+              leading: const Icon(Icons.brightness_3),
+              title: Text('Moon'),
+              subtitle: Text('cold and white'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: (){
+                print('moon');
+              },
+                     ),
+           ),
+           Card(
+             elevation: 20,
+             child: ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Stars'),
+              subtitle: Text('sstar'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: (){
+                print('card');
+              },
+                     ),
+           )
+        ],
+      ),
     );
   }
 }
