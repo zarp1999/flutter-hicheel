e884acabdaab1b4faba60b9d5c0e3a6b9ac00439
zarp1999 <zarp.191@gmail.com>
2022-03-01 08:12:24 +0800

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
-}
-
+} 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter layout demo'),
-          ),
-          body: ListView(
-            children: const [
-               ListTile(
-                title: Text('Үл хөдлөх'),
-                subtitle: Text('15.332 зар'),
-                leading: Icon(Icons.drag_indicator),
-              ),
-              ListTile(
-                title: Text('Ul hudluh'),
-                subtitle: Text('15.332 zar'),
-                leading: Icon(Icons.home),
-              ),
-              ListTile(
-                title: Text('Үл хөдлөх'),
-                subtitle: Text('15.332 зар'),
-                leading: Icon(Icons.home),
-              ),
-              ListTile(
-                title: Text('Ul hudluh'),
-                subtitle: Text('15.332 zar'),
-                leading: Icon(Icons.home),
-              ),
-            ],
-          )),
+    return Container(
+      color: Colors.black,
     );
   }
 }
